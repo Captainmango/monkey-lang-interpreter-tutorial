d1f26d6e7f5b3383eaf925fba45758ade4f9283c
Edward Heaver <edward.heaver01@gmail.com>
2023-11-10 13:31:41 +0000

    fixed it... forgot to return tok 😅

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -58,10 +58,13 @@ func (l *Lexer) NextToken() token.Token {
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
+
 			return tok
 		} else if isDigit(l.ch) {
-			tok.Type = token.INT
 			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			
+			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}

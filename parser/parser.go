648570cd100e70a06fbae802e2eda38134759975
Edward Heaver <edward.heaver01@gmail.com>
2023-11-11 18:52:11 +0000

    return statement

--- parser/parser.go
+++ parser/parser.go
@@ -61,6 +61,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -86,6 +88,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// @TODO: Sort out expressions (read to SEMICOLON)
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}

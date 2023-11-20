e92d90f235a5c33d53e178fa470e6b2e59ba68e4
Edward Heaver <edward.heaver01@gmail.com>
2023-11-20 23:25:01 +0000

    prefix expression for bang and minus

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -201,6 +201,7 @@ func TestParsingPrefixExpressions(t *testing.T) {
 		p := New(l)
 		program := p.ParseProgram()
 		checkParserErrors(t, p)
+		
 		if len(program.Statements) != 1 {
 			t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
 			1, len(program.Statements))

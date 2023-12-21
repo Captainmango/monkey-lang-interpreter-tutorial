e646a2b65c9d8c005a14020388eea94ecca6815e
Edward Heaver <edward.heaver01@gmail.com>
2023-12-21 18:08:30 +0000

    if statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -324,6 +324,88 @@ func TestBooleanExpression(t *testing.T) {
 	}
 }
 
+func TestIfExpression(t *testing.T) {
+	input := `if (x < y) { x }`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt, _ := assertProgram(t, *program)
+
+	exp, ok := stmt.Expression.(*ast.IfExpression)
+
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.IfExpression. got=%T",
+		stmt.Expression)
+	}
+
+	if !assertInfixExpression(t, exp.Condition, "x", "<", "y") {
+		return
+	}
+	if len(exp.Consequence.Statements) != 1 {
+		t.Errorf("consequence is not 1 statements. got=%d\n",
+		len(exp.Consequence.Statements))
+	}
+	consequence, ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("Statements[0] is not ast.ExpressionStatement. got=%T",
+		exp.Consequence.Statements[0])
+	}
+
+	if !assertIdentifier(t, consequence.Expression, "x") {
+		return
+	}
+
+	if exp.Alternative != nil {
+		t.Errorf("exp.Alternative.Statements was not nil. got=%+v", exp.Alternative)
+	}
+}
+
+func TestIfElseExpression(t *testing.T) {
+	input := `if (x < y) { x } else { y }`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt, _ := assertProgram(t, *program)
+
+	exp, ok := stmt.Expression.(*ast.IfExpression)
+	
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.IfExpression. got=%T",
+		stmt.Expression)
+	}
+
+	if !assertInfixExpression(t, exp.Condition, "x", "<", "y") {
+		return
+	}
+	if len(exp.Consequence.Statements) != 1 {
+		t.Errorf("consequence is not 1 statements. got=%d\n",
+		len(exp.Consequence.Statements))
+	}
+	consequence, ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("Statements[0] is not ast.ExpressionStatement. got=%T",
+		exp.Consequence.Statements[0])
+	}
+
+	if !assertIdentifier(t, consequence.Expression, "x") {
+		return
+	}
+
+	alternative, ok := exp.Alternative.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("Statements[0] is not ast.ExpressionStatement. got=%T",
+		exp.Alternative.Statements[0])
+	}
+
+	if !assertIdentifier(t, alternative.Expression, "y") {
+		return
+	}
+}
+
 func assertProgram(t *testing.T, p ast.Program) (*ast.ExpressionStatement, bool) {
 	if len(p.Statements) != 1 {
 		t.Fatalf("program has not enough statements. got=%d", len(p.Statements))

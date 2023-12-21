3526e0441a9e1a565c1544c64ebbd94ffd18c36e
Edward Heaver <edward.heaver01@gmail.com>
2023-12-21 18:24:42 +0000

    function literals and tests

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,12 @@
 package ast
+
 /*
 How the language works. AST Nodes make up expressions and statements in the programming language
 This will be turned into executable instructions
 */
 import (
 	"bytes"
+	"strings"
 
 	"github.com/Captainmango/monkey/token"
 )
@@ -219,6 +221,31 @@ func (ie *IfExpression) String() string {
 		out.WriteString("else ")
 		out.WriteString(ie.Alternative.String())
 	}
+
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token token.Token // The 'fn' token
+	Parameters []*Identifier
+	Body *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	params := []string{}
+	
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }

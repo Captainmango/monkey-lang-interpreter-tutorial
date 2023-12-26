54025961e55ce57cd5eaecb4be38bea96175b6df
Edward Heaver <edward.heaver01@gmail.com>
2023-12-26 19:44:38 +0000

    bool eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/Captainmango/monkey/object"
 )
 
+var (
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL = &object.Null{}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -13,6 +19,8 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBoolObj(node.Value)
 	}
 
 	return nil
@@ -26,4 +34,12 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBoolObj(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+
+	return FALSE
 }
--- repl/repl.go
+++ repl/repl.go
@@ -23,7 +23,6 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-
 		fmt.Println(PROMPT)
 		
 		scanned := scanner.Scan()

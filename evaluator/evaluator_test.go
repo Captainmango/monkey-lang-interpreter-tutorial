71c15a6db6bb867d50e59386ca34b76b01810c58
Edward Heaver <edward.heaver01@gmail.com>
2023-12-31 19:55:26 +0000

    Creating objects and storing refs to them in our environment

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -40,8 +40,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 	
-	return Eval(program)
+	return Eval(program, env)
 }
 
 func testIntegerObj(t testing.TB, obj object.Object, want int64) bool {
@@ -233,6 +234,10 @@ func TestErrorHandling(t *testing.T) {
 			`,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
 	for _, tt := range tests {
@@ -249,5 +254,20 @@ func TestErrorHandling(t *testing.T) {
 			t.Errorf("wrong error message. expected=%q, got=%q",
 			tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+	for _, tt := range tests {
+		testIntegerObj(t, testEval(tt.input), tt.expected)
 	}
 }
